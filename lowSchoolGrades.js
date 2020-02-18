f4becbf910b7ed8dc692f87cd2996ff04ac4686a
MMkkff12 <mark.k.farrell@gmail.com>
2020-02-18 12:49:17 -0600

    paper grader lowSchoolGrades.js added

---
+++ lowSchoolGrades.js
@@ -0,0 +1,19 @@
+var a = 65;
+
+function score(grade){
+    if (grade >= 90){
+    console.log("A");
+    }
+    else if (grade >= 80){
+    console.log("B");
+    }
+    else if (grade >= 70){
+    console.log("C");
+    }
+    else if (grade >= 60){
+    console.log("D");
+    }
+    else
+        console.log("F");
+}
+score(a);
