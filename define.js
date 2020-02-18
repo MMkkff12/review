cbea6068ef6fbc3d8b389dd16d9face0bb4a17c3
MMkkff12 <mark.k.farrell@gmail.com>
2020-02-18 11:10:00 -0600

    added define and worksheet

---
+++ define.js
@@ -0,0 +1,21 @@
+//? Define All Words
+
+
+// while Loop used with a conditional expression to continue a process until the value changes
+// src SOURCE used in HTML with SCRIPT to find the " ".js to run
+// case upper or lower case is sensitive in java script
+// camel camelcase is where the first letter is lower case but words are added with upper case first letter
+    // Example upperCaseFirstLetter
+// script  is the behavior description in a program
+// expression evaluates to values, they can be strings, boolean etc
+// decision conclusion of a statement used with if, else or while
+// faster opposite of slower
+// var Variable they are used to store values
+// behavior many statements create the behavior of your page
+// bottles containers for beer
+// semicolon used at the end of statements
+// consolelog used to print results for java script in the terminal
+// statement a small part of a computation you want preformed by java script or HTML
+// conditional  creates a boolean such as >, <, !=
+// values what varables store
+// strings usually in quote marks Java script will usually consider undefined varables as strings.
